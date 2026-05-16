e0e6130b5bba3f440e2a9d35f82c383142a4f1bf
donarazhar <donarazhar@gmail.com>
2026-05-16 11:26:57 +0700

    Update logout redirect to landing page

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -122,7 +122,7 @@ class AuthController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect()->route('login')
+        return redirect()->route('home')
             ->with('success', 'Anda telah berhasil logout.');
     }
 
